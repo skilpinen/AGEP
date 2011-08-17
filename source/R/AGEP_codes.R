5866ee13260791249ea30b36bd7d6852b29fb3a2
skilpinen <sami.kilpinen@medisapiens.fi>
2011-08-17 20:11:22 +0300

    Added gene weight extension

--- source/R/AGEP_codes.R
+++ source/R/AGEP_codes.R
@@ -3,13 +3,14 @@
 # Author: Sami Kilpinen & Kalle Ojala
 ###############################################################################
 
-create.ref.obj<-function(data,annotation,min.data=5,min.bw=-Inf,max.bw=Inf,resolution=512,verbose=TRUE){
+create.ref.obj<-function(data,annotation,min.data=5,min.bw=-Inf,max.bw=Inf,resolution=512,verbose=TRUE,tos=NULL,return.expr=TRUE){
 	# Wrapper function to generate reference data object
 	if (ncol(data)!=length(annotation)){stop("Annotation vector needs to be as long as there are columns in the datamatrix!")}
-	bws<-calculate.bws(data=data,gr=annotation,min.data=min.data,min.bw=min.bw,max.bw=max.bw,verbose=verbose)
-	ref.data.obj<-create.tissue.dist(data=data,gr=annotation,bws=bws,resolution=resolution,verbose=verbose)
+	bws<-calculate.bws(data=data,gr=annotation,min.data=min.data,min.bw=min.bw,max.bw=max.bw,verbose=verbose,tos=tos)
+	ref.data.obj<-create.tissue.dist(data=data,gr=annotation,bws=bws,resolution=resolution,verbose=verbose,return.expr=return.expr)
 	return(ref.data.obj)
 }
+
 
 do.match<-function(query.sample,query.annotation=NULL,query.id=NULL,ref.data.obj,verbose=TRUE,verbose.res=FALSE){
 	# Wrapper function to match one sample against reference data (object)
@@ -19,20 +20,31 @@ do.match<-function(query.sample,query.annotation=NULL,query.id=NULL,ref.data.obj
 	return(match.res)
 }
 
-calculate.bws<-function(data,gr,min.data=5,min.bw=-Inf,max.bw=Inf,verbose){
+calculate.bws<-function(data,gr,min.data=5,min.bw=-Inf,max.bw=Inf,verbose=FALSE,tos=NULL){
 	un.groups<-unique(gr)
 	res<-vector("list",length=length(un.groups))
 	names(res)<-un.groups
 	
-	if (verbose){cat("\r Finding maximum values of each variable in each class (can take awhile)\n");flush.console()}
-	# Change NA values temporarily to -Inf 
-	na.ii<-is.na(data)
-	data[na.ii]<--Inf
-	maxes<-aggregate(t(data),by=list(gr),FUN=max,na.rm=T)
-	# Return those to NA
-	data[na.ii]<-NA
-	row.names(maxes)<-maxes[,1]
-	maxes<-as.matrix(maxes[,-1])
+	if (is.null(tos)){
+		if (verbose){cat("\r Finding maximum values of each variable in each class (can take awhile)\n");flush.console()}
+		# Change NA values temporarily to -Inf 
+		na.ii<-is.na(data)
+		data[na.ii]<--Inf
+		if (length(unique(gr))>1){
+			maxes<-aggregate(t(data),by=list(gr),FUN=max,na.rm=T)
+			# Return those to NA
+			data[na.ii]<-NA
+			row.names(maxes)<-maxes[,1]
+			maxes<-as.matrix(maxes[,-1])
+		} else {
+			maxes<-as.matrix(apply(data,1,max,na.rm=T))
+			row.names(maxes)<-row.names(data)
+			# Return those to NA
+			data[na.ii]<-NA
+		}
+	} else {
+		maxes<-tos
+	}
 	
 	# Calculate bws
 	for (i in 1:length(un.groups)){
@@ -57,11 +69,24 @@ calculate.bws<-function(data,gr,min.data=5,min.bw=-Inf,max.bw=Inf,verbose){
 	}
 	
 	# Calculate tos
-	tmp.frame<-t(data.frame(res))
-	row.names(tmp.frame)<-names(res)
-	df<-as.matrix(tmp.frame)
-	tmp<-(df*3)+maxes[row.names(df),]
-	tos<-apply(tmp,2,max,na.rm=T)
+	if (is.null(tos)){
+		if (length(unique(gr))>1){
+			tmp.frame<-t(data.frame(res))
+			row.names(tmp.frame)<-names(res)
+		} else {
+			tmp.frame<-data.frame(res)
+		}
+		
+		df<-as.matrix(tmp.frame)
+		tmp<-(df*3)+maxes[row.names(df),]
+		if (length(unique(gr))>1){
+			tos<-apply(tmp,2,max,na.rm=T)
+		} else {
+			tos<-tmp[,1]
+		}
+	} else {
+		tos<-maxes
+	}
 	if (verbose){
 		cat("\n")
 		flush.console()
@@ -69,7 +94,7 @@ calculate.bws<-function(data,gr,min.data=5,min.bw=-Inf,max.bw=Inf,verbose){
 	return(list(bws=res,tos=tos))
 }
 
-create.tissue.dist<-function(data,gr,bws,resolution=512,verbose){	
+create.tissue.dist<-function(data,gr,bws,resolution=512,verbose,return.expr=TRUE){	
 	tissue.dist<-list()
 	un.groups<-unique(gr)
 	scaling.factors<-list()
@@ -107,7 +132,11 @@ create.tissue.dist<-function(data,gr,bws,resolution=512,verbose){
 	names(scaling.factors)<-as.character(un.groups)
 	names(tissue.dist)<-as.character(un.groups)
 	names(from)<-names(to)
-	return(list(data.densities=tissue.dist,froms=from,tos=to,scaling.factors=scaling.factors,resolution=resolution,bws=bws,ref.data=data,annotation=gr))
+	if (return.expr){
+		return(list(data.densities=tissue.dist,froms=from,tos=to,scaling.factors=scaling.factors,resolution=resolution,bws=bws,ref.data=data,annotation=gr))
+	} else {
+		return(list(data.densities=tissue.dist,froms=from,tos=to,scaling.factors=scaling.factors,resolution=resolution,bws=bws,ref.data=NA,annotation=gr))
+	}
 }
 
 kd.fast<-function(nmbrs,x,bw){
@@ -121,7 +150,7 @@ kd.fast<-function(nmbrs,x,bw){
 }
 
 match.query<-function(query,gr.que,que.sample.id,ref.data.obj,verbose.res=TRUE,verbose=FALSE){
-
+	
 	gr.ref<-ref.data.obj$annotation
 	un.groups<-unique(gr.ref)
 	
@@ -180,10 +209,70 @@ match.query<-function(query,gr.que,que.sample.id,ref.data.obj,verbose.res=TRUE,v
 	final.scores<-sort(apply(res.computed,1,mean,na.rm=T), decreasing = TRUE)
 	
 	if (verbose.res){		
-		return(list(tm.scores=1-t(res),ts.scores=res.computed,query.id=que.sample.id,query.annotation=gr.que,tissue.scores=final.scores))
+		return(list(tm.scores=t(res),ts.scores=res.computed,query.id=que.sample.id,query.annotation=gr.que,tissue.scores=final.scores))
 	}
 	else {
 		return(list(query.id=que.sample.id,query.annotation=gr.que,tissue.scores=final.scores))
 	}
 }
+
+# Function for weighing the AGEP results with gene/class specific weights
+weighByGenes <- function (match.object, hit.genes, weight = 1, exponent = 2) {
+	
+	# Result vector
+	results <- c()
+	
+	# Loop across all tissue of the result
+	for (i in 1:nrow(match.object$ts.scores)) {
+		
+		# Which genes are common to both hit.genes and the AGEP result object
+		hit.index  <- which(names(hit.genes) == rownames(match.object$ts.scores)[i])
+		
+		# Where are these in the 
+		hit.these  <- match(colnames(match.object$ts.scores), names(hit.genes[[hit.index]]))
+		
+		# Calculate multiplier, balanced
+		multiplier <- (hit.genes[[hit.index]])[hit.these] ^ exponent / mean((hit.genes[[hit.index]])[hit.these] ^ exponent, na.rm = TRUE) * weight + (1 - weight)
+		
+		# Calculate results with gene multipliers applied
+		results[i] <- mean((match.object$ts.scores[i,]) * multiplier, na.rm = TRUE)
+	}
+	
+	# Give names to categories
+	names(results) <- rownames(match.object$ts.scores)
+	
+	# Resturn results
+	return(results)
+}
+
+hitGenes <- function (ref.obj, verbose = TRUE) {
+	require(bigmemory)
+	require(biganalytics)
+	
+	b <- big.matrix(length(ref.obj$data.densities),prod(dim(ref.obj$data.densities[[1]])))
+	for (i in 1:length(ref.obj$data.densities)){
+		b[i,]<-as.vector(ref.obj$data.densities[[i]])
+	}
+	
+	hit.genes<-lapply(1:length(ref.obj$data.densities),function(i){
+				cat("\r",i)
+				b[i,]<-0				
+				maxes<-colmax(b,na.rm=T)
+				
+				bi<-as.vector(ref.obj$data.densities[[i]])
+				b[i,]<-bi
+				
+				tmp.mat<-bi-maxes
+				
+				dim(tmp.mat)<-dim(ref.obj$data.densities[[i]])
+				
+				tmp.mat[tmp.mat<0]<-0
+				tmp.mat<-sapply(1:ncol(tmp.mat),function(y){tmp.mat[,y]*(ref.obj$tos[y]/512)})
+				colnames(tmp.mat)<-names(ref.obj$tos)
+				sort(colSums(tmp.mat,na.rm=T))
+			})
+	names(hit.genes)<-names(ref.obj$data.densities)
+	return(hit.genes)
+}
+
 
